7e3eaa4d784302a77e3e4e4fb0b8a1570cd3c4e1
Latika <Latikamw@google.com>
2022-10-06 20:22:52 +0000

    Renaming Variables

--- includes/s2_merge.js
+++ includes/s2_merge.js
@@ -9,14 +9,14 @@ function mergeScript(source,target) {
   where ${dataform.projectConfig.vars.timestampfield} > cast("${dataform.projectConfig.vars.executionDate}" as Date)) S
   ON   T.${dataform.projectConfig.vars.target_hash_unique_col_name}  =  S.hash_src_unique
       AND T.${dataform.projectConfig.vars.target_hash_non_unique_col_name} = S.hash_src_nonunique   
-      AND T.${dataform.projectConfig.vars.start_at_column_name} IS NULL  
-  WHEN NOT MATCHED BY SOURCE AND t.${dataform.projectConfig.vars.start_at_column_name} is null THEN
+      AND T.${dataform.projectConfig.vars.end_at_column_name} IS NULL  
+  WHEN NOT MATCHED BY SOURCE AND t.${dataform.projectConfig.vars.end_at_column_name} is null THEN
   UPDATE 
-  SET t.${dataform.projectConfig.vars.start_at_column_name} = 
+  SET t.${dataform.projectConfig.vars.end_at_column_name} = 
           case 
-              when t.${dataform.projectConfig.vars.start_at_column_name} is NULL then
+              when t.${dataform.projectConfig.vars.end_at_column_name} is NULL then
           (select max(${dataform.projectConfig.vars.timestampfield}) from ${(source)})
-              else t.${dataform.projectConfig.vars.start_at_column_name}
+              else t.${dataform.projectConfig.vars.end_at_column_name}
           end   
   WHEN NOT MATCHED BY TARGET THEN
     INSERT(	
